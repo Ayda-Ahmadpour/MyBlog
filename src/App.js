ce8e88778d188014441ca36de67b8e87382944d6
ayda <146557313+Ayda-Ahmadpour@users.noreply.github.com>
2024-02-19 11:30:36 -0500

    create pages and routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import SignIn from "./pages/SignIn/SignIn";
+import Home from "./pages/Home/Home";
+import SignUp from "./pages/SignUp/SignUp";
+import Dashboard from "./pages/Dashboard/Dashboard";
 
 function App() {
   return (
-    <>
-      <h3 className="text-3xl text-red-500">Hello</h3>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/Signin" element={<SignIn />} />
+        <Route path="/SignUp" element={<SignUp />} />
+        <Route path="/Dashboard" element={<Dashboard />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
